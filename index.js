250e788c8de6e443d17057ade71cd25480860d47
X <>
2021-02-22 20:33:53 +0000

    Update pageviews and price when slider is moved

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const slider = document.querySelector(".slider");
+const pageviewsFigure = document.querySelector(".pageviews-figure");
+const priceFigure = document.querySelector(".price-figure");
+const pageviewsOptions = ["10k", "50k", "100k", "250k", "500k", "1M"];
+const priceOptions = ["$4.00", "$8.00", "$16.00", "$24.00", "$32.00"];
+
+updatePageviews();
+updatePrice();
 
 slider.addEventListener("input", function () {
   slider.style.background = `linear-gradient(to right, var(--softCyan) ${
@@ -6,4 +13,15 @@ slider.addEventListener("input", function () {
   }%, var(--lighterGreyishBlue) ${
     ((slider.value - slider.min) * 100) / (slider.max - slider.min)
   }%)`;
+
+  updatePageviews();
+  updatePrice();
 });
+
+function updatePageviews() {
+  pageviewsFigure.innerHTML = pageviewsOptions[slider.value - 1];
+}
+
+function updatePrice() {
+  priceFigure.innerHTML = priceOptions[slider.value - 1];
+}
