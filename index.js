076f3e62d7e050ab137d67ff893f54f7864848a4
X <>
2021-02-22 21:12:54 +0000

    Add functionality to billing toggle

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const pageviewsFigure = document.querySelector(".pageviews-figure");
 const priceFigure = document.querySelector(".price-figure");
 const pageviewsOptions = ["10k", "50k", "100k", "250k", "500k", "1M"];
 const priceOptions = ["$4.00", "$8.00", "$16.00", "$24.00", "$32.00"];
+const billingToggle = document.querySelector(".billing-toggle");
 
 updatePageviews();
 updatePrice();
@@ -25,3 +26,7 @@ function updatePageviews() {
 function updatePrice() {
   priceFigure.innerHTML = priceOptions[slider.value - 1];
 }
+
+billingToggle.addEventListener("click", function () {
+  billingToggle.classList.toggle("billing-toggle--active");
+});
